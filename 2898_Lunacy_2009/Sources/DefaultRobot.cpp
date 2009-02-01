c572a3dfe9477cde8d5ae9f9be40d9da54f08a45
Steven Garcia <Dreglor@gmail.com>
2009-02-01 09:00:18 +0000

    Quick update to secondary systems well more like a total update :P

--- 2898_Lunacy_2009/Sources/DefaultRobot.cpp
+++ 2898_Lunacy_2009/Sources/DefaultRobot.cpp
@@ -2,10 +2,13 @@
 
 /**
  * This is a demo program showing the use of the RobotBase class.
- * The SimpleRobot class is the base of a robot application that will automatically call your
+ * The SimpleRobot class get food for hot plate and bring back to me
+ * is the base of a robot application that will automatically call your
  * Autonomous and OperatorControl methods at the right time as controlled by the switches on
  * the driver station or the field controls.
  */
+
+int x;
 class DefaultRobot : public SimpleRobot
 {
 	RobotDrive *myRobot;			// robot drive system
@@ -15,11 +18,39 @@ class DefaultRobot : public SimpleRobot
 	Joystick *leftStick;			// joystick 2 (tank left stick)
 	Joystick *armStick;				// joystick 3 to control arm
 	DriverStation *ds;				// driver station object
+	Victor *myrightvictor;
+	Victor *myleftvictor;
+	Victor *mytopspinner;
+	Victor *myconveyerbelt;
+	Solenoid *myhatch;
+	Relay *mybottomspinner;
+	Relay *mycompressor;
+	DigitalInput *mycheckball;
+	
 
 	enum							// Driver Station jumpers to control program operation
 	{ ARCADE_MODE = 1,				// Tank/Arcade jumper is on DS Input 1 (Jumper present is arcade)
 	  ENABLE_AUTONOMOUS = 2,		// Autonomous/Teleop jumper is on DS Input 2 (Jumper present is autonomous)
-	} jumpers;	                            
+	} jumpers;	                          
+	enum
+	{                                //all fake values
+		rightdrive = 1,
+		leftdrive = 2,
+		conveyerbelt = 3,
+		topspinner = 4,
+		bottomspinner =4,
+		compressor =6,
+		openhatch =7
+		
+	}pwms;
+	enum                              //all fake values
+	{
+		gatherswitch=1,
+		upswitch=2,
+		downswitch=4,
+		hatchswitch =6
+		
+	}controllerinputs;
 
 public:
 	/**
@@ -32,14 +63,22 @@ public:
 	DefaultRobot(void)
 	{
 		ds = DriverStation::GetInstance();
-		myRobot = new RobotDrive(1, 3, 2, 4);	// create robot drive base
+		myRobot = new RobotDrive(rightdrive, leftdrive);
 		rightStick = new Joystick(1);			// create the joysticks
 		leftStick = new Joystick(2);
-		armStick = new Joystick(3);
-		armUpperLimit = new DigitalInput(1);	// create the limit switch inputs
-		armLowerLimit = new DigitalInput(2);
+// these channel are all controlled by enum but may need slot val
+		mycheckball = new DigitalInput(x);
+		myrightvictor = new Victor(rightdrive);
+		mytopspinner  = new Victor(topspinner);
+		myleftvictor = new Victor(leftdrive);
+		myconveyerbelt  = new Victor(conveyerbelt);
+		mycompressor = new Relay(compressor);
+		mybottomspinner = new Relay(bottomspinner);
+		myhatch = new Solenoid(openhatch);
+
 		//Update the motors at least every 100ms.
 		GetWatchdog().SetExpiration(100);
+		
 	}
 
 	/**
@@ -64,35 +103,72 @@ public:
 	 */
 	void OperatorControl(void)
 	{
-		Victor armMotor(5);						// create arm motor instance
+		bool gathervariable = 0;
+		int beltstatus = 0; 
+		/*The beltstatus variable is to keep track of how the belt is set to be driven
+		 * 1 = up -1 =down 0 = neutral*/
 		while (IsOperatorControl())
 		{
 			GetWatchdog().Feed();
 
 			// determine if tank or arcade mode; default with no jumper is for tank drive
-			if (ds->GetDigitalIn(ARCADE_MODE) == 0) {	
+			if (ds->GetDigitalIn(ARCADE_MODE) == 0) 
+			{	
 				myRobot->TankDrive(leftStick, rightStick);	 // drive with tank style
-			} else{
+			} 
+			else
+			{
 				myRobot->ArcadeDrive(rightStick);	         // drive with arcade style (use right stick)
 			}
-
-			// Control the movement of the arm using the joystick
-			// Use the "Y" value of the arm joystick to control the movement of the arm
-			float armStickDirection = armStick->GetY();
-
-			// if at a limit and telling the arm to move past the limit, don't drive the motor
-			if ((armUpperLimit->Get() == 0) && (armStickDirection > 0.0)) {
-				armStickDirection = 0;
-			} else if ((armLowerLimit->Get() == 0) && (armStickDirection < 0.0)) {
-				armStickDirection = 0;
+			// beginning of the 2898 secondary system code
+			if (ds->GetDigitalIn(gatherswitch) == true)       //check to turn on gather
+			{
+				gathervariable = 1;
+				//todo relay output here
 			}
-
-			// Set the motor value 
-			armMotor.Set(armStickDirection);
+			else
+			{
+				gathervariable =0;
+				//todo relay stop
+			}
+			if (ds->GetDigitalIn(upswitch) == true)
+			{
+				myconveyerbelt->Set(.5);  // move belt up
+				beltstatus = 1;
+			}
+			else {
+				myconveyerbelt->Set(0);  //stop belt
+				beltstatus = 0;
+			}
+			if (ds->GetDigitalIn(downswitch) == true && ds->GetDigitalIn(upswitch)==false)
+			{ //if down is push and up is not pushed, proceed
+				myconveyerbelt->Set(-.5); //set belt down
+				beltstatus = -1;
+			}
+			else
+			{
+				myconveyerbelt->Set(0); //stop belt
+			beltstatus = 0;
+			}
+			if (gathervariable == true)
+			{
+				if (mycheckball->Get() == true) //unsure if "get" works
+				{
+					myconveyerbelt->Set(.5);
+					Wait(1);
+					myconveyerbelt->Set(beltstatus * .5);
+				}
+			}
+			if (ds->GetDigitalIn(hatchswitch) == true)
+				{
+				//Todo: learn how to stop relay
+				}
+			else
+			 	{
+				//Todo: learn how to stop relay
+				
+			 	}
 		}
 	}
 };
-
-START_ROBOT_CLASS(DefaultRobot);
-
 
